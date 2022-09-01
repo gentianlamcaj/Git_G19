199a39dbb4edcd8e9280fb1823175da6fd7b2c76
Emre YILMAZ <107490353+emcemen@users.noreply.github.com>
2022-09-01 08:42:57 -0400

    I added new method and it solved

--- src/MethodsHere.java
+++ src/MethodsHere.java
@@ -2,4 +2,36 @@ public class MethodsHere {
     /*
     please follow Guzel Worklow and paste your methods here at the end of the file
      */
+
+    public static void main(String[] args) {
+        prefixAgain("abXYabc",1);
+    }
+
+
+
+    /*
+      Given a string, consider the prefix string made of the first N chars of the string. Does that prefix string
+    appear somewhere else in the string? Assume that the string is not empty and that N is in the range 1..str.length().
+    prefixAgain("abXYabc", 1) → true
+    prefixAgain("abXYabc", 2) → true
+    prefixAgain("abXYabc", 3) → false
+     */
+
+    // Here is my solution for the question
+
+    public static boolean prefixAgain(String str, int n) {
+        if(str.length()>1){
+            if(str.substring(n,str.length()-1).contains(str.substring(0,n))){
+                return true;}
+        }if(str.length()==2){
+            if(str.substring(0,1).contains(str.substring(n))){
+                return true;}
+        }else{
+            return false;
+        }
+        return false;
+
+    }//methods end here
 }
+
+
