052a577f7711210b1d49e1f6f130c03f6b8a61eb
Beyza Gedemenli <beyzagedemenli@gmail.com>
2022-09-10 14:59:52 -0400

    Created doubleChar method in MethodsHere.java

--- src/MethodsHere.java
+++ src/MethodsHere.java
@@ -49,6 +49,23 @@ public class MethodsHere {
         return append;
     }
 
+    /*
+        Beyza:
+            Given a string, return a string where for every char in the original, there are two chars.
+    */
+    public String doubleChar(String str) {
+
+        String result="";
+
+        for(int i = 0; i < str.length(); i++){
+            char letter = str.charAt(i);
+            result += "" + letter + letter;
+        }
+
+        return result;
+    }
+
+
 }
 
 
