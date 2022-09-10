678130b9e19b58828cdbd7ab2de543e35a663f7d
Guzel Khisamieva <guzel.khisamieva89@gmail.com>
2022-09-10 18:53:48 -0400

    solution is added

--- src/MethodsHere.java
+++ src/MethodsHere.java
@@ -107,8 +107,11 @@ sum28([1, 2, 3, 4]) → false
 
     public boolean sum28(int[] nums) {
         int sum=0;
-
-
+        for (int i=0; i<nums.length; i++){
+            if (nums[i]==2){
+                sum+=nums[i];
+            }
+        }
         return (sum==8);
     }
 
